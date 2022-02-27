10eec53fd602ec1919bfc490ce479a9dfd50665d
bazzdamage <bazzdamage@outlook.com>
2022-02-28 02:33:16 +0700

    Homework Seminar01

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -5,23 +5,27 @@ public class Seminar01
 
     public static void Main(String[] args)
     {
-
+        int count = 0;
         int a = 115;
         int b = 18;
         int c = 955;
         dosimpleMath(a, b);
         domanualMath(a, b);
         maxOfThree(a, b, c);
+        count = countOfInput();
+        int[] numsarray = getArrayofInt(count);
+        foreach (int i in numsarray) Console.Write(i + ", ");
+        Console.WriteLine("Even Numbers are: " + evenNums(numsarray));
+        Console.WriteLine(printAllEvenNums());
     }
-
     private static void dosimpleMath(int a, int b)
     {
-        Console.WriteLine("Максимальное из двух чисел = " + Math.Max(a, b));
+        Console.WriteLine("(Variant 1) Max Of 2 nums are: " + Math.Max(a, b));
     }
     private static void domanualMath(int a, int b)
     {
-        if (a < b) Console.WriteLine("Максимальное из двух чисел = " + b);
-        else Console.WriteLine("Максимальное из двух чисел = " + a);
+        if (a < b) Console.WriteLine("(Variant 2) Max Of 2 nums are: " + b);
+        else Console.WriteLine("(Variant 2) Max Of 2 nums are: " + a);
     }
     private static void maxOfThree(int a, int b, int c)
     {
@@ -29,8 +33,57 @@ public class Seminar01
         if (a > b) max = a;
         if (b > max) max = b;
         if (c > max) max = c;
-
-        Console.WriteLine("Максимальное из трёх чисел = " + max);
-
+        Console.WriteLine("Max Of 3 nums are: " + max);
     }
+    private static int countOfInput()
+    {
+        Console.WriteLine("How big Array do you want?: ");
+        int count = userinputINT();
+        return count;
+    }
+    private static int[] getArrayofInt(int count)
+    {
+       int[] array = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine("Please fill Array [ " + (i+1) + "/" + count + " ]" + ": ");
+            array[i] = userinputINT();
+        }
+        return array;
+    
+    }
+    private static int userinputINT()
+    {
+        int num = 0;
+        bool result = false;
+        while (result == false)
+        {
+            Console.WriteLine("Number MUST be integer: ");
+            string s = Console.ReadLine();
+            result = int.TryParse(s, out num);            
+        }
+
+        return num;
+    }
+    private static string evenNums(int[] array)
+    {
+        string result = "";
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0) result = result + array[i] + " ";
+        }
+        return result;
+    }
+    private static string printAllEvenNums()
+    {
+        Console.WriteLine("Please enter N: ");
+        int n = userinputINT();
+        string result = "";
+        for (int i = 2; i < n; i+=2)
+        {
+            result = result + i + " ";
+        }
+        return result;
+    }
+
 }
