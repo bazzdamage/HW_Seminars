ee7bbf2e2b2a48e6517461863cecdebfe06165f4
bazzdamage <bazzdamage@outlook.com>
2022-02-28 03:08:25 +0700

    Fix visual

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -5,29 +5,31 @@ public class Seminar01
 
     public static void Main(String[] args)
     {
-        int count = 0;      //int count for Array
         int a = 115;    
         int b = 18;         //2 ints for Problem_01 (Max of 2 Nums)
         int c = 955;        //int for Problem_02 (Max of 3 Nums)
-        dosimpleMath(a, b);     //Solution Method for Problem_01
-        domanualMath(a, b);     //Alt. Solution Method for Problem_01
-        maxOfThree(a, b, c);        //Solution Method for Problem_02
-        count = countOfInput();     //get count of nums in Array from user
-        int[] numsarray = getArrayofInt(count);     //get Array of custom size and user input for nums
+        
+        DoSimpleMath(a, b);     //Solution Method for Problem_01
+        DoAltMath(a, b);     //Alt. Solution Method for Problem_01
+        MaxOfThree(a, b, c);        //Solution Method for Problem_02
+
+        int count = CountOfInput();     //get count of nums in Array from user
+        int[] numsarray = GetArrayOfInt(count);     //get Array of custom size and user input for nums
+
         foreach (int i in numsarray) Console.Write(i + ", ");       //print Array
-        Console.WriteLine("\n" + "Even Numbers are: " + evenNums(numsarray));       //print string with only even nums in array (Problem_03)
-        Console.WriteLine(printAllEvenNums());      // Solution for Problem_04
+        Console.WriteLine("\n" + "Even Numbers are: " + EvenNums(numsarray));       //print string with only even nums in array (Problem_03)
+        Console.WriteLine(PrintAllEvenNums());      // Solution for Problem_04
     }
-    private static void dosimpleMath(int a, int b)
+    private static void DoSimpleMath(int a, int b)
     {
         Console.WriteLine("(Variant 1) Max Of 2 nums are: " + Math.Max(a, b));
     }
-    private static void domanualMath(int a, int b)
+    private static void DoAltMath(int a, int b)
     {
         if (a < b) Console.WriteLine("(Variant 2) Max Of 2 nums are: " + b);
         else Console.WriteLine("(Variant 2) Max Of 2 nums are: " + a);
     }
-    private static void maxOfThree(int a, int b, int c)
+    private static void MaxOfThree(int a, int b, int c)
     {
         int max = 0;
         if (a > b) max = a;
@@ -35,23 +37,23 @@ public class Seminar01
         if (c > max) max = c;
         Console.WriteLine("Max Of 3 nums are: " + max);
     }
-    private static int countOfInput()
+    private static int CountOfInput()
     {
         Console.WriteLine("How big Array do you want?: ");
-        int count = userinputINT();
+        int count = UserInputINT();
         return count;
     }
-    private static int[] getArrayofInt(int count)
+    private static int[] GetArrayOfInt(int count)
     {
        int[] array = new int[count];
         for (int i = 0; i < count; i++)
         {
             Console.WriteLine("Please fill Array [ " + (i+1) + "/" + count + " ]" + ": ");
-            array[i] = userinputINT();
+            array[i] = UserInputINT();
         }
         return array;    
     }
-    private static int userinputINT()
+    private static int UserInputINT()
     {
         int num = 0;
         bool result = false;
@@ -63,7 +65,7 @@ public class Seminar01
         }
         return num;
     }
-    private static string evenNums(int[] array)
+    private static string EvenNums(int[] array)
     {
         string result = "";
         for (int i = 0; i < array.Length; i++)
@@ -72,10 +74,10 @@ public class Seminar01
         }
         return result;
     }
-    private static string printAllEvenNums()
+    private static string PrintAllEvenNums()
     {
         Console.WriteLine("Please enter N: ");
-        int n = userinputINT();
+        int n = UserInputINT();
         string result = "";
         for (int i = 2; i <= n; i+=2)
         {
