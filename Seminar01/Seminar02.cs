b7039c4f023bb23432b625cd33cd523f837453ab
bazzdamage <bazzdamage@outlook.com>
2022-03-07 00:38:39 +0700

    Add Solution for the Problem 13

--- Seminar01/Seminar02.cs
+++ Seminar01/Seminar02.cs
@@ -10,9 +10,10 @@ namespace Seminar01
     {
         public static void Seminar02Solution()
         {
-            ReverseNum();
+            //ReverseNum(); //Solution for The Problem 10
+            //PrintThirdNum(); //Solution for the Problem 13
         }
-         private static void ReverseNum()
+        private static void ReverseNum()
         {
             int revnum = 0;
             int count = 0;
@@ -34,7 +35,36 @@ namespace Seminar01
             }
             Console.WriteLine("revnum: " + revnum);
             Console.WriteLine("count: " + count);
-        }        
-        
+        }
+
+        private static void PrintThirdNum()
+        {
+            int thirdnum = 0;
+            int count = 0;
+            int num = Utility.UserInputINT();
+            int temp = num;
+
+            while (temp > 0)
+            {
+                temp = temp / 10;
+                count++;
+            }
+
+            temp = num;
+
+            if (count < 3) Console.WriteLine("Number of digits < 3, nothing to print");
+            else
+            {
+                while (num > 0)
+                {
+                    thirdnum = num % 10;
+                    num = num / 10;
+                    if (count == 3) Console.WriteLine("Third digit in the " + temp + " is: " + thirdnum);
+                    count--;
+                    
+                }
+            }
+        }
+
     }
 }
