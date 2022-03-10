c49de7d943f40a075b6a0051587c08fc3e69d0bb
bazzdamage <bazzdamage@outlook.com>
2022-03-11 02:50:09 +0700

    Add Seminar04 Solution for Future

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -8,6 +8,7 @@ public class Solution
         //Seminar01.Seminar01Solution();
         //Seminar02.Seminar02Solution();
         Seminar03.Seminar03Solution();
+        //Seminar04.Seminar04Solution();
     }
     
 }
---
+++ Seminar01/Seminar04.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminars
+{
+    internal class Seminar04
+    {
+        public static void Seminar04Solution()
+        {
+
+        }
+    }
+}
